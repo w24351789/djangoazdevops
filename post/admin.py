680b036d69d90492e11160bf7c9a656135ccc0a8
Ted Tai 戴瑋德 <ted.tai@cloudriches.com>
2022-03-23 15:15:57 +0800

    first commit

---
+++ post/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
---
+++ post/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
